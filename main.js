453818f97654fd486fbd675e06316cdbcb9a9ba2
D4Q2 <61206792+D4Q2@users.noreply.github.com>
2021-02-20 13:41:24 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+var options = {
+  //host: 'private-github-api.com', // <-- Private github api url. If not passed, defaults to 'api.github.com'
+  //pathPrefix: 'prefix-for-enterprise-instance', // <-- Private github api url prefix. If not passed, defaults to null.
+  protocol: 'https', // <-- http protocol 'https' or 'http'. If not passed, defaults to 'https'
+  user: 'D4Q2', // <-- Your Github username
+  repo: 'database', // <-- Your repository to be used a db
+  remoteFilename: 'database.json' // <- File with extension .json
+};
+ 
+// Require GithubDB
+var GithubDB = require('..').default;
+// Initialize it with the options from above.
+var githubDB = new GithubDB(options);
+ 
+// Authenticate Github DB -> grab a token from here https://github.com/settings/tokens
+githubDB.auth(personalAccessToken);
+ 
+// Connect to repository
+githubDB.connectToRepo();
+ 
+// You are now authenticated with Github and you are ready to use it as your database.
+githubDB.save({"message": "wooohoo"});
